c425ba2c2e0546c32ecaa10ce8824ddd26e1ea93
Brian Eyster <eysterbrian@gmail.com>
2020-09-28 14:50:15 -0600

    Adds toppings and slug to pizza schema

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -18,6 +18,15 @@ export default {
       description: 'Name of the pizza',
     },
     {
+      name: 'slug', // API id
+      title: 'Slug', // display name
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
       name: 'image', // API id
       title: 'Image of the Pizza', // display name
       type: 'image',
@@ -31,17 +40,40 @@ export default {
       type: 'number',
       description: 'Price of the pizza in cents',
       // Don't let anyone add an unreasonable cheap pizza...
-      validation: (Rule) => Rule.required().min(5000).precision(0).positive(),
+      validation: (Rule) => Rule.required().min(500).precision(0).positive(),
       // TODO: Add custom input component in Sanity Studio
     },
     {
-      name: 'slug', // API id
-      title: 'Slug', // display name
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 100,
-      },
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'topping' }],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
     },
   ],
+  preview: {
+    select: {
+      title: 'title',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      console.log(toppings);
+      return {
+        title,
+        media,
+        subtitle: Object.values(toppings)
+          .filter((topping) => topping)
+          .join(', '),
+      };
+    },
+  },
 };
