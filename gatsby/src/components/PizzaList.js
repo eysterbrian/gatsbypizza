9a3f186cb78bdf9154cbcd27fcc2761b30efd5bd
Brian Eyster <eysterbrian@gmail.com>
2020-09-30 15:20:16 -0600

    Applies styles to PizzaList and SinglePizza
    - The styled.div for the pizza list uses CSS subgrid.
    - Since subgrid isn't supported yet in chrome, we use an @supports
    query to provide an override style when subgrid doesn't exist

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,25 +1,50 @@
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 import React from 'react';
+import styled from 'styled-components';
+
+const PizzaListStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+
+  /* In case subgrid isn't supported by browser, provide fallback style */
+  @supports not (grid-template-rows: subgrid) {
+    --subgridrows: auto auto 1fr;
+  }
+
+  /* Use subgrid to provide row sizing from the parent's grid */
+  grid-template-rows: var(--subgridrows, subgrid);
+  grid-row: span 3;
+  gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 function SinglePizza({ pizza }) {
   return (
-    <div>
+    <PizzaStyles>
       <Link to={`/pizza/${pizza.slug.current}`}>
         <span className="mark">{pizza.name}</span>
-        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
-        <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
       </Link>
-    </div>
+      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+    </PizzaStyles>
   );
 }
 
 export default function PizzaList({ pizzas }) {
   return (
-    <ul>
+    <PizzaListStyles>
       {pizzas.map((pizza) => (
         <SinglePizza key={pizza.id} pizza={pizza} />
       ))}
-    </ul>
+    </PizzaListStyles>
   );
 }
