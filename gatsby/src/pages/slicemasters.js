1a624d9f41443a99f2f6ebc7a61f9991d5feade1
Brian Eyster <eysterbrian@gmail.com>
2020-10-01 17:23:13 -0600

    Displays all people in sliceMasters page

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,11 +1,79 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react';
-import Layout from '../components/Layout';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
 
-export default function SliceMastersPage() {
+const SliceMasterGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 2rem;
+`;
+
+const SliceMasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    z-index: 2;
+    position: relative;
+  }
+  p {
+    transform: rotate(1deg);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -8rem;
+    width: 70%;
+    background: var(--yellow);
+    z-index: 2;
+    position: relative;
+    text-align: center;
+  }
+`;
+
+export default function SliceMastersPage({ data }) {
+  console.log(data.slicemasters);
   return (
-    <>
-      <h1>Slice Masters</h1>
-      <p>This is the slice masters page!</p>
-    </>
+    <SliceMasterGridStyles>
+      {data.slicemasters.nodes.map((slicemaster) => (
+        <SliceMasterStyles>
+          <Link to={`/slicemaster/${slicemaster.slug.current}`}>
+            <h2>
+              <span className="mark">{slicemaster.name}</span>
+            </h2>
+            <Img fluid={slicemaster.image.asset.fluid} />
+            <p className="description">{slicemaster.description}</p>
+          </Link>
+        </SliceMasterStyles>
+      ))}
+    </SliceMasterGridStyles>
   );
 }
+
+export const query = graphql`
+  query MyQuery {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        description
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        slug {
+          current
+        }
+      }
+    }
+  }
+`;
