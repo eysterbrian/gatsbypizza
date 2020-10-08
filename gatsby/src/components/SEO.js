8cd4440a5381d3544c0801b1d30a39520409ef39
Brian Eyster <eysterbrian@gmail.com>
2020-10-08 14:07:19 -0600

    Adds meta and other head tags to SEO

--- gatsby/src/components/SEO.js
+++ gatsby/src/components/SEO.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
-export default function SEO({ title }) {
+export default function SEO({
+  children,
+  title,
+  location,
+  description,
+  image = '',
+}) {
   // Get the name of the website
   const {
     site: { siteMetadata },
@@ -18,10 +24,32 @@ export default function SEO({ title }) {
     }
   `);
 
+  const descriptionTxt = description || siteMetadata.description;
+
   return (
     <Helmet titleTemplate={siteMetadata.titleTemplate}>
       <html lang="en" />
       <title>{title}</title>
+      {/* Fav Icons */}
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="alternate icon" href="/favicon.ico" />
+      {/* Potentially unnecessary tags (Gatsby probably provides these already) */}
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta charset="utf-8" />
+      {/* SEO Meta tags */}
+      <meta name="description" content={descriptionTxt} />
+      {/* Open Graph tags */}
+      {location && <meta property="og:url" content={location.href} />}
+      <meta property="og:image" content={image || '/logo.svg'} />
+      <meta property="og:title" content={title} key="ogtitle" />
+      <meta
+        property="og:site_name"
+        content={siteMetadata.title}
+        key="ogsitename"
+      />
+      <meta property="og:description" content={descriptionTxt} key="ogdesc" />
+      {/* Allow child components here to overwrite any Helmet tags */}
+      {children}
     </Helmet>
   );
 }
