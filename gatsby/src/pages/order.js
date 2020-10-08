2bb67eeea261f2e188612c3109b96f1191f4d35c
Brian Eyster <eysterbrian@gmail.com>
2020-10-08 16:34:14 -0600

    Begins creation of order form
    - Currently this gets to a rough list of pizzas with no styling

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,13 +1,85 @@
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
-import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
-export default function OrderPage() {
+export default function OrderPage({ data }) {
+  const [values, updateHandler] = useForm({
+    email: '',
+    name: '',
+  });
+
+  const pizzas = data.pizzas.nodes;
+
   return (
     <>
       <SEO title="Order a Pizza" />
-      <h1>Order</h1>
-      <p>This is the order page</p>
+      <form>
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={values.name}
+            onChange={updateHandler}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={values.email}
+            onChange={updateHandler}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+          {pizzas.map((pizza) => (
+            <div key={pizza.id}>
+              <Img
+                width="50"
+                height="50"
+                fluid={pizza.image.asset.fluid}
+                alt={pizza.name}
+              />
+              <div>
+                <h2>{pizza.name}</h2>
+                <ul>
+                  <li>Prices</li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
     </>
   );
 }
+
+export const query = graphql`
+  query AllPizzasQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        image {
+          asset {
+            fluid(maxWidth: 200) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        slug {
+          current
+        }
+      }
+    }
+  }
+`;
