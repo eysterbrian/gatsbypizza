38af9797cff236659475f7cb4f006251472ff304
Brian Eyster <eysterbrian@gmail.com>
2020-10-21 11:17:20 -0600

    placeOrder uses EJS to parse separate MJML file
    - Previously, I was copying the MJML code into parseOrder's
    generateOrderEmail() fn.  But that seemed messy!
    - So I now load the email.mjml file using fs.readFile.
    - But then in order to do the interpolation of the variables in the
    mjml file (for order items, order total), I had to bring in a template
    engine.
    - So I rewrote those interpolation parts of the MJML file usng
    EJS syntax.
    
    Content of file changes:
    - Installed ejs packages to the placeOrder package.json
    - Changed email.mjml to use EJS syntax
    - placeOrder's generateOrderEmail now calls f.readFileSync and
    ejs.parse prior to mjml2html.

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,7 @@
 const nodemailer = require('nodemailer');
 const mjml2html = require('mjml');
+const fs = require('fs');
+const ejs = require('ejs');
 
 // Create a transport for nodemailer
 const transporter = nodemailer.createTransport({
@@ -19,41 +21,22 @@ const transporter = nodemailer.createTransport({
 function generateOrderEmail(order, total) {
   console.log('Inside generateOrder...');
 
+  let mjmlTemplate;
+  try {
+    const data = fs.readFileSync(require.resolve('./email.mjml'));
+    mjmlTemplate = data.toString();
+  } catch (err) {
+    console.log(err);
+  }
+
+  // Parse the EJS template values
+  const mjml = ejs.render(mjmlTemplate, { order, total });
+  console.log('MJML =====', mjml);
+
   // Destructure the return value which is MJMLParseResults object
-  const { html, errors } = mjml2html(`
-  <mjml>
-  <mj-head>
-    <mj-title>Your Order from Slick's Slices</mj-title>
-  </mj-head>
-  <mj-body background-color="white">
-    <mj-section>
-      <mj-column>
-        <mj-text font-size="20px">Your recent order for ${total}</mj-text>
-        <mj-text>We'll have your order ready in the next 20 minutes!</mj-text>
-      </mj-column>
-    </mj-section>
-    <mj-section>
-      <mj-column>
-        <mj-table>
-          ${order
-            .map(
-              (item) => `<tr>
-            <td><img src="${item.thumbnail}" alt="${item.name}"></td>
-            <td>${item.size} ${item.name} - ${item.price}</td>
-          </tr>`
-            )
-            .join('')}
-        </mj-table>
-      </mj-column>
-    </mj-section>
-    <mj-section>
-      <mj-column>
-        <mj-text>Your order total of <strong>${total}</strong> is due at pickup </mj-text>
-      </mj-column>
-    </mj-section>
-  </mj-body>
-</mjml>
-  `);
+  const { html, errors } = mjml2html(mjml);
+
+  console.log('HTML ======', html);
 
   if (errors) {
     console.log(errors.formattedMessage);
