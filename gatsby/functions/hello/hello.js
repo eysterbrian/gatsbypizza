438f654a5eaf34edc10b6033e6a614dcb6e29c2c
Brian Eyster <eysterbrian@gmail.com>
2020-10-22 14:46:34 -0600

    Adds hello API function for testing purposes

--- gatsby/functions/hello/hello.js
+++ gatsby/functions/hello/hello.js
@@ -2,6 +2,6 @@ exports.handler = async (event, context) => {
   console.log(event);
   return {
     statusCode: 200,
-    body: JSON.stringify('Hello!!!!'),
+    body: JSON.stringify({ event, context }),
   };
 };
