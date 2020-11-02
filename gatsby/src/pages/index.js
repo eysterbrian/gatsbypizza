26e5200fffa337e3b692c266ed1f30c7ecffe102
Brian Eyster <eysterbrian@gmail.com>
2020-11-02 14:27:26 -0700

    Adds the loading grid when waiting for API response

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import { HomepageGrids, ItemsGrid } from '../components/Grids';
+import LoadingGrid from '../components/LoadingGrid';
 import useLatestData from '../utils/useLatestData';
 
 function CurrentlySlicing({ slicemasters }) {
   console.log(slicemasters);
   return (
     <div>
-      <p>Currently Slicing</p>
-      <ul>
+      <h2 className="mark">SliceMasters on duty</h2>
+      <p>Ready for action!</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters.length && <p>No one working right now!</p>}
+      <ItemsGrid>
         {slicemasters &&
           slicemasters.map((slicemaster) => (
-            <li key={slicemaster.id}>{slicemaster.name}</li>
+            <div key={slicemaster.id}>{slicemaster.name}</div>
           ))}
-      </ul>
+      </ItemsGrid>
     </div>
   );
 }
@@ -20,11 +24,14 @@ function CurrentlySlicing({ slicemasters }) {
 function HotSlices({ hotSlices }) {
   return (
     <div>
-      <p>Hot out of the oven!</p>
-      <ul>
+      <h2 className="mark">Hot out of the oven!</h2>
+      <p>Come on and buy 'em by the slice</p>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices.length && <p>Nothin' in the case</p>}
+      <ItemsGrid>
         {hotSlices &&
-          hotSlices.map((slice) => <li key={slice.id}>{slice.name}</li>)}
-      </ul>
+          hotSlices.map((slice) => <div key={slice.id}>{slice.name}</div>)}
+      </ItemsGrid>
     </div>
   );
 }
@@ -38,10 +45,10 @@ export default function HomePage() {
     <div className="center">
       <h1>The best pizza around!</h1>
       <p>Open every day from 11am-11pm</p>
-      <div>
+      <HomepageGrids>
         <CurrentlySlicing slicemasters={slicemasters} />
         <HotSlices hotSlices={hotSlices} />
-      </div>
+      </HomepageGrids>
     </div>
   );
 }
