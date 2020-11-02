6f3b0ba20d301274a648578ca24c73856dba8320
Brian Eyster <eysterbrian@gmail.com>
2020-11-02 12:59:49 -0700

    Fetches client's StoreSettings from API
    - new custom hook in useLatestData fetches and
    stores the data.
    - Uses fetch for the graphql endpoint (ugly!)
    - main home page in index.js now calls this custom
    hook and passes the data along to 2 new
    sub-components HotSlices and CurrentlySlicing
    - TODO: Fetch all the relevant data for slices and
    slicemasters.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing({ slicemasters }) {
+  console.log(slicemasters);
+  return (
+    <div>
+      <p>Currently Slicing</p>
+      <ul>
+        {slicemasters &&
+          slicemasters.map((slicemaster) => (
+            <li key={slicemaster.id}>{slicemaster.name}</li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+function HotSlices({ hotSlices }) {
+  return (
+    <div>
+      <p>Hot out of the oven!</p>
+      <ul>
+        {hotSlices &&
+          hotSlices.map((slice) => <li key={slice.id}>{slice.name}</li>)}
+      </ul>
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { hotSlices, slicemasters } = useLatestData();
+
+  console.log({ hotSlices, slicemasters });
+
   return (
-    <>
-      {' '}
-      <p> Hey, I'm the Home page </p>
-    </>
+    <div className="center">
+      <h1>The best pizza around!</h1>
+      <p>Open every day from 11am-11pm</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
   );
 }
---
+++ gatsby/src/utils/useLatestData.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+export default function useLatestData() {
+  // hot slices
+  const [hotSlices, setHotSlices] = useState();
+  // slicemasters
+  const [slicemasters, setSlicemasters] = useState();
+
+  useEffect(() => {
+    // Fetch the data when the component loads
+    // TODO: Fetch all the relevant data here - including images
+    fetch(process.env.GATSBY_SANITY_GRAPHQL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+	StoreSettings(id: "MainStore") {
+    storeName
+    slicemaster {
+      name
+      _id
+    }
+    hotSlices {
+      name
+      _id
+    }
+  }
+}        
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // Check for errors
+
+        // NOTE that each of these setXXX methods will trigger a re-render.  So we'll
+        // end up with 2 re-renders here with the 1st render only having data for slicemasters
+        // since we won't yet have run setHotSlices
+        setSlicemasters(res.data.StoreSettings.slicemaster);
+        setHotSlices(res.data.StoreSettings.hotSlices);
+      });
+  }, []);
+
+  return { hotSlices, slicemasters };
+}
